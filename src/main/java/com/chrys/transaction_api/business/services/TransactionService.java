00792d1cfcf4afe841fa2f6865152864dd65fa3d
chryswds <dybas.chrys@gmail.com>
2025-01-25 00:11:40 +0000

    feat: addTransaction method - logs

--- src/main/java/com/chrys/transaction_api/business/services/TransactionService.java
+++ src/main/java/com/chrys/transaction_api/business/services/TransactionService.java
@@ -1,7 +1,9 @@
 package com.chrys.transaction_api.business.services;
 
 import com.chrys.transaction_api.controller.dtos.RequestTransactionDTO;
+import com.chrys.transaction_api.infrastructure.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -10,9 +12,24 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransactionService {
 
     private final List<RequestTransactionDTO> transactions = new ArrayList<>();
 
+    public void addTransaction(RequestTransactionDTO dto) {
 
+        log.info("Transaction process started");
+
+        if(dto.timestamp().isAfter(OffsetDateTime.now())) {
+            log.error("Date and time cannot be in the future");
+            throw new UnprocessableEntity("cannot be in the future");
+        }
+        if(dto.value() < 0){
+            log.error("value cannot be a negative value");
+            throw new UnprocessableEntity("cannot be negative");
+        }
+
+        transactions.add(dto);
+    }
 }
