4490b46ae2698c21ee95110ccfc72a0e2ccd018c
chryswds <dybas.chrys@gmail.com>
2025-01-25 00:52:59 +0000

    DELETE Method - to delete transactions

--- src/main/java/com/chrys/transaction_api/controller/TransactionController.java
+++ src/main/java/com/chrys/transaction_api/controller/TransactionController.java
@@ -5,10 +5,7 @@ import com.chrys.transaction_api.controller.dtos.RequestTransactionDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transaction")
@@ -21,6 +18,12 @@ public class TransactionController {
     public ResponseEntity<Void> addTransaction(@RequestBody RequestTransactionDTO dto) {
         transactionService.addTransaction(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTransaction() {
+        transactionService.deleteTransaction();
+        return ResponseEntity.ok().build();
     }
 }
 
