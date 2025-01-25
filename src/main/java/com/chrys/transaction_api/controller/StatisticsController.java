c42ce062be010456338736db70a909155a89eae1
chryswds <dybas.chrys@gmail.com>
2025-01-25 01:20:20 +0000

    swagger added

--- src/main/java/com/chrys/transaction_api/controller/StatisticsController.java
+++ src/main/java/com/chrys/transaction_api/controller/StatisticsController.java
@@ -2,8 +2,12 @@ package com.chrys.transaction_api.controller;
 
 import com.chrys.transaction_api.business.services.StatisticsService;
 import com.chrys.transaction_api.controller.dtos.StatisticsResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,12 @@ public class StatisticsController {
 
     private final StatisticsService statisticsService;
 
+    @GetMapping
+    @Operation(description = "Endpoint responsible for searching transactions statistics")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Statistics found successfully"),
+            @ApiResponse(responseCode = "400", description = "Could not search for statistics"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<StatisticsResponseDTO> getStatistics(@RequestParam(value = "Search Range", required = false, defaultValue = "60") Integer Timestamp) {
         return ResponseEntity.ok(statisticsService.getStatistics(Timestamp));
     }
--- src/main/java/com/chrys/transaction_api/controller/TransactionController.java
+++ src/main/java/com/chrys/transaction_api/controller/TransactionController.java
@@ -2,6 +2,9 @@ package com.chrys.transaction_api.controller;
 
 import com.chrys.transaction_api.business.services.TransactionService;
 import com.chrys.transaction_api.controller.dtos.RequestTransactionDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +18,23 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping
+    @Operation(description = "Endpoint responsible for adding transactions")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Transaction added successfully"),
+    @ApiResponse(responseCode = "422", description = "Fields not meeting application requirements"),
+    @ApiResponse(responseCode = "400", description = "Could not process the request"),
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Void> addTransaction(@RequestBody RequestTransactionDTO dto) {
         transactionService.addTransaction(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsible for deleting transactions")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Transaction deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "Could not process the request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Void> deleteTransaction() {
         transactionService.deleteTransaction();
         return ResponseEntity.ok().build();
